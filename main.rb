eb3621b2fa6c871fff2c7606db5f105dc804349d
phyushin <phyushin@gmail.com>
2016-03-17 06:42:23 +0000

    added ability to load from YAML config file

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 require 'httparty'
 require 'json'
+require 'optparse'
+require 'yaml'
+
 require_relative 'hs_session'
 
-base_url = "http://127.0.0.1:3000"
-api_path = "/v1/hs_session"
+config = YAML.load_file('config.yaml')
+
+base_url = config['base_url'] #"http://127.0.0.1:3000"
+api_path = config['api_path'] #"/v1/hs_session"
 puts base_url + api_path
 
 hs_sess = Hs_Session.new("FFFFFF","2016-03-01")
