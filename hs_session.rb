c763cc32432462b0443e3a62346bf1351c4695fb
phyushin <phyushin@gmail.com>
2016-03-17 08:04:17 +0000

    added post function to post the UID and the timestamp to the HMS system

--- hs_session.rb
+++ hs_session.rb
@@ -1,15 +1,20 @@
+require 'httparty'
 class Hs_Session
   def initialize(uid,current_time)
     @uid =uid
     @current_time = current_time
   end
 
-  def getUid()
+  def GetUid()
     @uid
   end
 
-  def getCurrent_time()
+  def GetCurrent_Time()
     @current_time
+  end
+
+  def Post(url,apikey)
+    @result = HTTParty.post(url.to_s, :body=>{:uid =>@uid}.to_json, :headers=>{'Content-Type'=>'application/json'})
   end
 end
 
--- main.rb
+++ main.rb
@@ -4,12 +4,15 @@ require 'optparse'
 require 'yaml'
 
 require_relative 'hs_session'
+require_relative 'create_config'
 
 config = YAML.load_file('config.yaml')
 
 base_url = config['base_url'] #"http://127.0.0.1:3000"
 api_path = config['api_path'] #"/v1/hs_session"
-puts base_url + api_path
+api_key = config['api_key']
+location = base_url + api_path
 
 hs_sess = Hs_Session.new("FFFFFF","2016-03-01")
-puts "the card#{hs_sess.getUid} signed in at #{hs_sess.getCurrent_time}"
+puts hs_sess.Post(location,api_key)
+puts "the card#{hs_sess.GetUid} signed in at #{hs_sess.GetCurrent_Time}"
