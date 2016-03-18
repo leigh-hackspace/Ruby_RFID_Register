f4d6db74d91e7df79ce316fcbe3278da73395839
phyushin <phyushin@gmail.com>
2016-03-18 20:15:20 +0000

    added Gemfile for bundle

--- main.rb
+++ main.rb
@@ -1,19 +1,28 @@
 require 'httparty'
 require 'json'
-require 'optparse'
 require 'yaml'
 
 require_relative 'hs_session'
 require_relative 'create_config'
 
-config = YAML.load_file('config.yaml')
+# main RFID Register Class
+class Register
+  def initialize
+    config = YAML.load_file('config.yaml')
+    @base_url = config['base_url']
+    @api_path = config['api_path']
+    @api_key = config['api_key']
+    @location = @base_url + @api_path
+  end
 
-base_url = config['base_url']
-api_path = config['api_path']
-api_key = config['api_key']
-location = base_url + api_path
+  def login(carduid, time)
+    hs_sess = HsSession.new(carduid, time)
+    result =  hs_sess.post(@location, @api_key)
+    'error' unless result['message'] == 'OK'
+    puts "the card #{hs_sess.uid} signed in at #{hs_sess.current_time}"
+  end
+end
 
-hs_sess = HsSession.new('FFFFFF', '2016-03-01')
-result =  hs_sess.post(location, api_key)
-'error' unless result['message'] == 'OK'
-puts "the card #{hs_sess.uid} signed in at #{hs_sess.current_time}"
+register = Register.new
+
+register.login('FFFFFF', Time.now.getutc)
