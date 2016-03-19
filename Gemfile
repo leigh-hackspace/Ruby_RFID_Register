e6bcfff78f0dd7735f9314c5bceb2ea56891335e
phyushin <phyushin@gmail.com>
2016-03-19 19:58:47 +0000

    inital working version

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 gem 'httparty'
 gem 'json'
-
+gem 'serialport', '~> 1.3', '>= 1.3.1'
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'httparty'
 require 'json'
 require 'yaml'
+require 'serialport'
 
 require_relative 'hs_session'
 require_relative 'create_config'
@@ -12,7 +13,18 @@ class Register
     @base_url = config['base_url']
     @api_path = config['api_path']
     @api_key = config['api_key']
-    @location = @base_url + @api_path
+    @port = config['com_port']
+    @location = config['base_url'] + config['api_path']
+    init_serial_params
+  end
+
+  def init_serial_params
+    @params = {
+      'baud' => 9600,
+      'data_bits' => 8,
+      'stop_bits' => 1,
+      'parity' => SerialPort::NONE
+    }
   end
 
   def login(carduid, time)
@@ -21,8 +33,19 @@ class Register
     'error' unless result['message'] == 'OK'
     puts "the card #{hs_sess.uid} signed in at #{hs_sess.current_time}"
   end
+
+  def read_serial
+    puts "Monitoring on #{@port}"
+    @sp = SerialPort.new(@port, @params)
+    @sp.read_timeout = 200
+    loop do
+      while (i = @sp.gets) do
+        login(JSON.parse(i)['CardUID'], Time.now.getutc)
+      end
+    end
+  end
+
 end
-
 register = Register.new
-
-register.login('FFFFFF', Time.now.getutc)
+register.read_serial
+#register.login('FFFFFF', Time.now.getutc)
