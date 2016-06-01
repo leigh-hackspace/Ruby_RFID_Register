a4972a557a78a38d5746e65e693a9d60fda01f4c
hackspacer <hackspacer@leighhack.org>
2016-06-01 10:08:09 +0000

    added pi piper gem for the move to the GPIO RFID method using SPI , added a new file to allow clean up of straggling sessions | Phyushin |

--- main.rb
+++ main.rb
@@ -2,9 +2,11 @@ require 'httparty'
 require 'json'
 require 'yaml'
 require 'serialport'
+require 'pi_piper'
 
 require_relative 'hs_session'
 require_relative 'create_config'
+include PiPiper
 
 # main RFID Register Class
 class Register
@@ -16,6 +18,8 @@ class Register
     @port = config['com_port']
     @location = config['base_url'] + config['api_path']
     init_serial_params
+#    @pin = PiPiper::Pin.new(:pin =>3, :direction => :out)
+#    @pin.off
   end
 
   def init_serial_params
@@ -31,6 +35,9 @@ class Register
     hs_sess = HsSession.new(carduid)
     result =  hs_sess.post(@location, @api_key)
     'error' unless result['message'] == 'OK'
+ #   @pin.on
+ #   sleep 1
+ #   @pin.off
   end
 
   def read_serial
